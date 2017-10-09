9b94ed8bf93562dc94c14c4d379ea936f98caab7
SunQ <sunq0011@gmail.com>
2017-10-10 03:30:48 +0900

    Signed-off-by: SunQ <sunq0011@gmail.com>

--- AngularWithSpring/src/main/java/org/angularstudy/spring/controller/ProductDataController.java
+++ AngularWithSpring/src/main/java/org/angularstudy/spring/controller/ProductDataController.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.angularstudy.spring.services.ObjectToXml;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
@@ -17,84 +18,84 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @RequestMapping("/angularjs/")
 public class ProductDataController {
 	private static final Logger logger = LoggerFactory.getLogger(ProductDataController.class);
-	
+
 	@RequestMapping("productData.json")
 	public @ResponseBody String getProductDataCtrl(Model model) throws Exception{
 		ArrayList<Map<String, Object>> arrayList = new ArrayList<Map<String, Object>>();
 		Map<String, Object> fruitInfoMap = null;
-		
+
 		for (int i = 0; i < 9; i++) {
 			fruitInfoMap = new HashMap<String, Object>();
 			switch (i) {
 			case 0:
-				fruitInfoMap.put("name", "Apples"); 	
-			 	fruitInfoMap.put("category", "Fruit"); 	
-			 	fruitInfoMap.put("price", 1.20); 	
+				fruitInfoMap.put("name", "Apples");
+			 	fruitInfoMap.put("category", "Fruit");
+			 	fruitInfoMap.put("price", 1.20);
 			 	fruitInfoMap.put("expiry", 10);
 			 	arrayList.add(fruitInfoMap);
 				break;
 			case 1:
-				fruitInfoMap.put("name", "Bananas"); 	
-				fruitInfoMap.put("category", "Fruit"); 	
-				fruitInfoMap.put("price", 2.42); 	
+				fruitInfoMap.put("name", "Bananas");
+				fruitInfoMap.put("category", "Fruit");
+				fruitInfoMap.put("price", 2.42);
 				fruitInfoMap.put("expiry", 7);
 				arrayList.add(fruitInfoMap);
 				break;
 			case 2:
-				fruitInfoMap.put("name", "Pears"); 	
-				fruitInfoMap.put("category", "Fruit"); 	
-				fruitInfoMap.put("price", 2.02); 	
+				fruitInfoMap.put("name", "Pears");
+				fruitInfoMap.put("category", "Fruit");
+				fruitInfoMap.put("price", 2.02);
 				fruitInfoMap.put("expiry", 6);
 				arrayList.add(fruitInfoMap);
 				break;
-			case 3:	
-				fruitInfoMap.put("name", "Tuna"); 	
-				fruitInfoMap.put("category", "Fish"); 		
-				fruitInfoMap.put("price", 20.45); 	
+			case 3:
+				fruitInfoMap.put("name", "Tuna");
+				fruitInfoMap.put("category", "Fish");
+				fruitInfoMap.put("price", 20.45);
 				fruitInfoMap.put("expiry", 3);
 				arrayList.add(fruitInfoMap);
-			case 4:	
-				fruitInfoMap.put("name", "Salmon"); 	
-				fruitInfoMap.put("category", "Fish"); 		
-				fruitInfoMap.put("price", 17.93); 	
+			case 4:
+				fruitInfoMap.put("name", "Salmon");
+				fruitInfoMap.put("category", "Fish");
+				fruitInfoMap.put("price", 17.93);
 				fruitInfoMap.put("expiry", 2);
 				arrayList.add(fruitInfoMap);
 				break;
-			case 5:	
-				fruitInfoMap.put("name", "Trout"); 	
-				fruitInfoMap.put("category", "Fish"); 		
-				fruitInfoMap.put("price", 12.93); 	
+			case 5:
+				fruitInfoMap.put("name", "Trout");
+				fruitInfoMap.put("category", "Fish");
+				fruitInfoMap.put("price", 12.93);
 				fruitInfoMap.put("expiry", 4);
 				arrayList.add(fruitInfoMap);
 				break;
 			case 6:
-				fruitInfoMap.put("name", "Beer"); 	
-				fruitInfoMap.put("category", "Drinks"); 	
-				fruitInfoMap.put("price", 2.99); 	
+				fruitInfoMap.put("name", "Beer");
+				fruitInfoMap.put("category", "Drinks");
+				fruitInfoMap.put("price", 2.99);
 				fruitInfoMap.put("expiry", 365);
 				arrayList.add(fruitInfoMap);
 				break;
-			case 7:	
-				fruitInfoMap.put("name", "Wine"); 	
-				fruitInfoMap.put("category", "Drinks"); 	
-				fruitInfoMap.put("price", 8.99); 	
+			case 7:
+				fruitInfoMap.put("name", "Wine");
+				fruitInfoMap.put("category", "Drinks");
+				fruitInfoMap.put("price", 8.99);
 				fruitInfoMap.put("expiry", 365);
 				arrayList.add(fruitInfoMap);
 				break;
-			case 8:	
-				fruitInfoMap.put("name", "Whiskey"); 	
-				fruitInfoMap.put("category", "Drinks"); 	
-				fruitInfoMap.put("price", 45.99); 	
+			case 8:
+				fruitInfoMap.put("name", "Whiskey");
+				fruitInfoMap.put("category", "Drinks");
+				fruitInfoMap.put("price", 45.99);
 				fruitInfoMap.put("expiry", 365);
 				arrayList.add(fruitInfoMap);
 				break;
 			}
 		}
-		
+
 		ObjectMapper mapper = new ObjectMapper();
 		String data = mapper.writeValueAsString(arrayList);
 		logger.info(data);
-		
+		logger.info("xml Object::::" + new ObjectToXml().executeObject());
 		return data;
 	}
 
---
+++ AngularWithSpring/src/main/java/org/angularstudy/spring/services/Answer.java
@@ -0,0 +1,42 @@
+package org.angularstudy.spring.services;
+
+public class Answer {
+
+	private int id;
+	private String answerName;
+	private String postedBy;
+
+	public Answer() {}
+
+	public Answer(int id, String answerName, String postedBy) {
+		super();
+		this.id = id;
+		this.answerName = answerName;
+		this.postedBy = postedBy;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getAnswerName() {
+		return answerName;
+	}
+
+	public void setAnswerName(String answerName) {
+		this.answerName = answerName;
+	}
+
+	public String getPostedBy() {
+		return postedBy;
+	}
+
+	public void setPostedBy(String postedBy) {
+		this.postedBy = postedBy;
+	}
+
+}
