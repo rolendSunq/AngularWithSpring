d07c9db31c9ecc7a5491bb13bd814da3704cca0e
SunQ <sunq0011@gmail.com>
2017-10-25 14:03:18 +0900

    ssg Signed-off-by: SunQ <sunq0011@gmail.com>

--- AngularWithSpring/src/main/java/org/angularstudy/spring/controller/ProductDataController.java
+++ AngularWithSpring/src/main/java/org/angularstudy/spring/controller/ProductDataController.java
@@ -5,13 +5,11 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.angularstudy.spring.dataobject.ProductVO;
 import org.angularstudy.spring.services.ObjectToXml;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
